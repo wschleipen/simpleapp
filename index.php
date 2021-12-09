fa005ff1bfcd931217930dc8ccae664e5c4a41cf
Winfried Schleipen <winfried.schleipen@de.ibm.com>
2021-12-09 07:16:09 +0100

    new line added

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <body> 
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action, slightly modified ;.-)</b></p>
+                <p>With a new line of text added here to demonstrate build updates</p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
